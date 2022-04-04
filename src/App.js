2b87d8265fe8662457e3eecd8734d29baea61b0a
ShokiYokota <syoki.yokota@azone-japan.co.jp>
2022-04-04 17:23:40 +0900

    prevstateの勉強

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import {useState} from 'react'
 
 
 function App() {
-  const [state,setState]  = useState("On")
+  const [state,setState]  = useState(false)
   const changeState =()=>{
-    setState ("OFF")
+    setState (prevState => !prevState)
   }
   return (
     <div>
-      今は{state}です
+      今は{state ? "ON":"OFF"}です
       <Article
         title={"タイトル表示"}
         content={"内容表示です"}
